b3f3a1c01da96de2cefaf95433fce0ba1f2f50da
negativenagesh <gaonkarsub@gmail.com>
2024-11-19 10:24:44 +0530

    created react web app

---
+++ arecanut-classification-website/src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="about-container">
+            <h1>About This Project</h1>
+            <p>
+                This project helps farmers classify the quality of arecanuts and predict market prices
+                using state-of-the-art machine learning techniques.
+            </p>
+        </div>
+    );
+};
+
+export default About;
