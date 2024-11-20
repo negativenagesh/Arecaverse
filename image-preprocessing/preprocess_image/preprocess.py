d0296fede36c06c633555bee60aed79994969a47
negativenagesh <gaonkarsub@gmail.com>
2024-11-20 14:37:44 +0530

    added all the path to all the image and preprocessed

---
+++ image-preprocessing/preprocess_image/preprocess.py
@@ -0,0 +1,5 @@
+from app.preprocessing import preprocess_images as pi
+
+# pi("D:/Arecanut-quality-classification/arecanut-images/1stchalipre","preprocessed-1stchali")
+# pi("D:/Arecanut-quality-classification/arecanut-images/gpre","preprocessed-gpre")
+pi("D:/Arecanut-quality-classification/arecanut-images/kolepre","preprocessed-kole")
